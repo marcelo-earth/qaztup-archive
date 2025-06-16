c0d283891076bbee717a0f3db0a33b0624338eff
Marcelo Arias <hello@marceloarias.com>
2025-06-15 23:14:41 -0500

    Add Vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  adapter: vercel({
+    webAnalytics: true,
+  }),
+});
